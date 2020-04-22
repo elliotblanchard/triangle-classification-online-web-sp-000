3affbb1eebe1ac0a1b621bdd3ed9469b7ba8a03d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-22 01:02:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,15 @@
 class Triangle
-  # write code here
-  def kind
+  attr_accessor :side1, :side2, :side3
 
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 == side2 && side2 = side3
+      :equilateral
   end
 
   class TriangleError < StandardError

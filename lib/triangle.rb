42a06c9f215960589c79842ab7ddeb7cc56cd96a
Elliot Blanchard <public@elliotblanchard.com>
2020-04-22 01:11:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,9 +10,9 @@ class Triangle
   end
 
   def kind
-    if side1 == side2 && side2 = side3 && side3 == side1
+    if @side1 == @side2 && @side2 = @side3 && @side3 == @side1
       :equilateral
-    elsif side1 == side2 || side2 == side3 || side1 == side3
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
     else
       binding.pry
